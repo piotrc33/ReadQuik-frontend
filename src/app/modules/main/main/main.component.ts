327af1116b97a4393bfedb47c4bbed2dc0424137
piotrc33 <potrek40@wp.pl>
2023-10-06 14:43:05 +0200

    First exercise base

--- src/app/modules/main/main/main.component.ts
+++ src/app/modules/main/main/main.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent{
+export class MainComponent {
+  phraseNumber: number = 0;
 
-  constructor(private readonly authService: AuthService, private router: Router) { }
+  bookFragments: string[] = [
+    'Miriamowi',
+    '(Zenonowi Przesmyckiemu)',
+    'I',
+    '— Niech będzie pochwalony',
+    'Jezus Chrystus!',
+    '— Na wieki wieków,',
+    '—moja Agato,',
+    'a dokąd to wędrujecie, co?',
+  ];
+
+  phrasesArray: string[] = []
+
+  constructor(
+    private readonly authService: AuthService,
+    private router: Router
+  ) {}
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === ' ') {
+      this.nextFragment();
+    }
+  }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
 
+  nextFragment() {
+    this.phraseNumber++;
+  }
 }
