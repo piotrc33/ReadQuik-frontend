af85bc482bb77d36c8cc325aad1f3fb8a48b1340
piotrc33 <potrek40@wp.pl>
2023-10-30 07:36:51 +0100

    Add button styles, logout button

--- src/app/features/modules/main/components/main/main.component.ts
+++ src/app/features/modules/main/components/main/main.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class MainComponent {
 
   constructor(
-    private readonly authService: AuthService,
+    public readonly authService: AuthService,
     private router: Router
   ) {}
 
