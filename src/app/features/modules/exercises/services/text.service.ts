4e2daedec5a368669904c931792401da9048f2fd
piotrc33 <potrek40@wp.pl>
2023-12-27 18:22:05 +0100

    Fix auto mode bug and exercise 4 not equal spaces bug

--- src/app/features/modules/exercises/services/text.service.ts
+++ src/app/features/modules/exercises/services/text.service.ts
@@ -3,18 +3,40 @@ import { SegmentI } from '../../../../api/model/segment.i';
 
 @Injectable()
 export class TextService {
-
-
   getFragmentsWithNewlines(text: string): string[] {
     let fragments: string[] = [];
     let piped: string = text.replace(/[,.?!;…]+/g, '$&|');
-    piped = piped.replace(/\S+$/gm, '$&|');
+    piped = piped.replace(/\S+$/gm, '$&|'); // adding pipe at the end
     piped = piped.replace(/(?<!^)—/gm, '$&|');
     fragments = this.splitByNewlines(piped);
     fragments = this.splitByPipes(fragments);
     fragments = this.splitLongFragments(fragments);
 
     return fragments;
+  }
+
+  splitByNewlines(text: string): string[] {
+    return text.split(/(\n)/);
+  }
+
+  splitByPipes(fragments: string[]): string[] {
+    let result: string[] = [];
+    fragments.forEach((frag) => {
+      const split = frag.split(/\|+/);
+      result = result.concat(split);
+    });
+
+    result = result.filter((el) => el !== '');
+    return result;
+  }
+
+  splitLongFragments(fragments: string[]): string[] {
+    let result: string[] = [];
+    fragments.forEach((frag) => {
+      const split = this.splitTextByNWords(frag, 4);
+      result = result.concat(split);
+    });
+    return result;
   }
 
   splitTextByNWords(text: string, n: number): string[] {
@@ -36,17 +58,20 @@ export class TextService {
     let segNumber: number = 1;
     const wps = 200; // words per segment
 
-    if(!words) {
+    if (!words) {
       return [];
     }
 
     for (let i = 0; i < words.length; i++) {
       segText.push(words[i]);
-      if(segText.length >= wps && this.isNewline(segText[segText.length - 1])) {
+      if (
+        segText.length >= wps &&
+        this.isNewline(segText[segText.length - 1])
+      ) {
         segments.push({
           number: segNumber,
-          text: segText.join('')
-        })
+          text: segText.join(''),
+        });
         segText = [];
         segNumber++;
       }
@@ -55,35 +80,11 @@ export class TextService {
     return segments;
   }
 
-  splitByNewlines(text: string): string[] {
-    return text.split(/(\n)/);
-  }
-
   isNewline(phrase: string): boolean {
     return /^\n+/.test(phrase);
   }
 
   removeNewlines(fragments: string[]): string[] {
     return fragments.filter((frag) => !this.isNewline(frag));
-  }
-
-  splitByPipes(fragments: string[]): string[] {
-    let result: string[] = [];
-    fragments.forEach((frag) => {
-      const split = frag.split(/\|+/);
-      result = result.concat(split);
-    });
-
-    result = result.filter((el) => el !== '');
-    return result;
-  }
-
-  splitLongFragments(fragments: string[]): string[] {
-    let result: string[] = [];
-    fragments.forEach((frag) => {
-      const split = this.splitTextByNWords(frag, 4);
-      result = result.concat(split);
-    });
-    return result;
   }
 }
