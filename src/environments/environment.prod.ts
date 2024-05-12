7f8a0b96d2bdd316e8636ea3585852c0ffccd1b9
piotrc33 <potrek40@wp.pl>
2024-05-12 13:16:57 +0200

    Small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  domain: 'https://readquik.onrender.com/',
+  domain: 'https://readquik.onrender.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  domain: 'http://localhost:3002/'
+  domain: 'http://localhost:3002'
 };
 
 /*
