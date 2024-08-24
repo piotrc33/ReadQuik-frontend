f3020fc0f730e4ed3ffcef68e6e4383cae795180
piotrc33 <potrek40@wp.pl>
2024-08-24 14:52:13 +0200

    Make exercise-info smart

--- src/app/features/exercises/components/exercises/exercises.component.ts
+++ src/app/features/exercises/components/exercises/exercises.component.ts
@@ -1,27 +1,16 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { BookService } from 'src/app/features/library/services/book.service';
-import { CurrentExerciseService } from 'src/app/shared/services/current-exercise.service';
 import { ExercisesProgressStateService } from 'src/app/shared/services/exercises-progress-state.service';
 import { ExerciseFlowService } from '../../services/exercise-flow.service';
-import { InstructionsService } from '../../services/instructions.service';
 
 @Component({
   selector: 'exercises',
   templateUrl: './exercises.component.html',
   styleUrls: ['./exercises.component.scss'],
 })
-export class ExercisesComponent implements OnInit {
-  readonly #bookService = inject(BookService);
+export class ExercisesComponent {
   readonly #progressState = inject(ExercisesProgressStateService);
-  readonly #currentExerciseService = inject(CurrentExerciseService);
   readonly #flowService = inject(ExerciseFlowService);
-  readonly #instructionService = inject(InstructionsService);
-  readonly exerciseNumber = this.#currentExerciseService.exerciseNumber;
-
-  ngOnInit(): void {
-    this.#currentExerciseService.initialExerciseNumberAction$.next();
-  }
 
   get unlockedExercisesCount(): number {
     return this.#progressState.unlockedExercisesCount();
@@ -35,31 +24,11 @@ export class ExercisesComponent implements OnInit {
     return this.#progressState.isCurrentExerciseUnlocked();
   }
 
-  get wordPhrases(): string[] {
-    return this.#bookService.wordPhrases();
-  }
-
   get exerciseOpened(): boolean {
     return this.#flowService.exerciseOpened();
   }
 
   startExercise() {
     this.#flowService.startExerciseAction$.next();
-  }
-
-  get instructionsOpened(): boolean {
-    return this.#instructionService.instructionsOpened();
-  }
-
-  get currentInstructionObject(): any {
-    return this.#instructionService.currentInstructionObject();
-  }
-
-  openInstructions() {
-    this.#instructionService.openInstructionsAction$.next(true);
-  }
-
-  closeInstructions() {
-    this.#instructionService.openInstructionsAction$.next(false);
   }
 }
--- src/app/features/exercises/components/ui/exercise-info/exercise-info.component.ts
+++ src/app/features/exercises/components/ui/exercise-info/exercise-info.component.ts
@@ -1,20 +1,50 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output, inject, input } from '@angular/core';
+import { BookService } from 'src/app/features/library/services/book.service';
+import { InstructionsService } from '../../../services/instructions.service';
+import { CurrentExerciseService } from 'src/app/shared/services/current-exercise.service';
 
 @Component({
   selector: 'exercise-info',
   templateUrl: './exercise-info.component.html',
   styleUrl: './exercise-info.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExerciseInfoComponent {
-  exerciseNumber = input<number>(1);
+export class ExerciseInfoComponent implements OnInit {
+  readonly #bookService = inject(BookService);
+  readonly #instructionService = inject(InstructionsService);
+  readonly #currentExerciseService = inject(CurrentExerciseService);
+
   repetitions = input<number>(0);
   isExerciseUnlocked = input<boolean>(false);
-  isTextLoaded = input<boolean>();
-
-  @Output()
-  instructionsOpened = new EventEmitter<void>();
 
   @Output()
   startedExercise = new EventEmitter<void>();
+
+  ngOnInit(): void {
+    this.#currentExerciseService.initialExerciseNumberAction$.next();
+  }
+
+  get isTextLoaded(): boolean {
+    return this.#bookService.wordPhrases().length > 0;
+  }
+
+  get exerciseNumber() {
+    return this.#currentExerciseService.exerciseNumber();
+  }
+
+  get currentInstructionObject(): any {
+    return this.#instructionService.currentInstructionObject();
+  }
+
+  get instructionsOpened(): boolean {
+    return this.#instructionService.instructionsOpened();
+  }
+
+  openInstructions() {
+    this.#instructionService.openInstructionsAction$.next(true);
+  }
+
+  closeInstructions() {
+    this.#instructionService.openInstructionsAction$.next(false);
+  }
 }
